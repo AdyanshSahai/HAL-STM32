91087f555d7876e51a8842923d43a24ee81f5508
Adyansh S <adyansh.sahai@gmail.com>
2026-05-18 19:33:09 +0100

    made the spacing pretty inside header file.

--- Inc/stm32f411.h
+++ Inc/stm32f411.h
@@ -65,14 +65,14 @@ typedef struct {
     volatile uint32_t AHB1RSTR;
     volatile uint32_t AHB2RSTR;
     volatile uint32_t AHB3RSTR;
-    uint32_t          RESERVED0;
+             uint32_t RESERVED0;
     volatile uint32_t APB1RSTR;
     volatile uint32_t APB2RSTR;
-    uint32_t          RESERVED1[2];
+             uint32_t RESERVED1[2];
     volatile uint32_t AHB1ENR;
     volatile uint32_t AHB2ENR;
     volatile uint32_t AHB3ENR;
-    uint32_t          RESERVED2;
+             uint32_t RESERVED2;
     volatile uint32_t APB1ENR;
     volatile uint32_t APB2ENR;
 } RCC_TypeDef;
